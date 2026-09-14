d8d6832e133142d071da7d41598d345a6157f820
hiroyukimaekawa-lang <hiroyuki.maekawa@crestix-inc.com>
2026-09-14 15:31:32 +0900

    Remove public GAS environment config

--- data/config.ts
+++ data/config.ts
@@ -1,10 +1,6 @@
-export const DEFAULT_GAS_URL =
-  'https://script.google.com/macros/s/AKfycbxoGBuRblAo7EYdij2fwr0yeHGkqZrlRfl2yOJ56hXX4wySn7EW4QwkyuREYyoL2jvn/exec';
-
 export const clinicConfig = {
   name: '水谷眼科診療所',
   homepageUrl: 'https://www.mizutani-eye-clinic.com/',
-  gasUrl: DEFAULT_GAS_URL,
   googleReviewUrl:
     process.env.NEXT_PUBLIC_GOOGLE_REVIEW_URL?.trim() ||
     'https://www.google.com/search?q=%E6%B0%B4%E8%B0%B7%E7%9C%BC%E7%A7%91%E8%A8%BA%E7%99%82%E6%89%80&oq=%E6%B0%B4%E8%B0%B7%E7%9C%BC%E7%A7%91%E8%A8%BA%E7%99%82%E6%89%80&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIHCAEQABiABDIHCAIQABiABDIGCAMQABgeMgYIBBAAGB4yBggFEEUYPTIGCAYQRRg9MgYIBxBFGD3SAQc0MTdqMGo3qAIAsAIA&sourceid=chrome&source=chrome.ob&ie=UTF-8#lrd=0x600376def9c14e19:0xc1e8d25a99428056,3,,,,',
